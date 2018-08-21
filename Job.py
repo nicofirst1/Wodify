d0d9406a2de255cc909e689f489cea8b8defb34f
Nick <brandimax2@gmail.com>
2018-08-21 17:03:37 +0200

    add play sound

--- Job.py
+++ Job.py
@@ -3,7 +3,7 @@ import random
 import threading
 import time
 
-from utils import time_print
+from utils import time_print, play_sound
 
 
 class Job(threading.Thread):
@@ -55,7 +55,7 @@ class Job(threading.Thread):
             random_wait=random.randint(self.freq[0],self.freq[1])*60
             while not self.stop.wait(timeout=random_wait):
                 random_rep=random.randint(self.rep[0],self.rep[1])
-                self.play_soud(repeat=2)
+                play_sound()
                 print(f"You must do {random_rep} {self.name}")
                 self.total+=random_rep
                 break
@@ -65,17 +65,4 @@ class Job(threading.Thread):
     def summary(self):
         to_print=f"Exercise : {self.name}\nFrequency : {self.freq}\nRepetitions : {self.rep}"
         return to_print
-
-
-    def play_soud(self,duration=1, freq=500, repeat=1):
-        """
-        Play a sound
-        :param duration: duration in seconds
-        :param freq: frequence of the sound
-        :return:
-        """
-
-        for i in range(repeat):
-            os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
-
 
